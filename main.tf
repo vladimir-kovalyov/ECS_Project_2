059243cf9dc1b93d8b251879c3f1a2a17af8bffe
Vladimirs Kovalovs <vladimirs.kovalovs@ecs-digital.co.uk>
2020-11-26 14:04:23 +0000

    Changed main.tf file

--- main.tf
+++ main.tf
@@ -2,24 +2,3 @@
 terraform {
     required_version = "~> 0.13.5"
 }
-
-##################################################################################
-# MODULE - NETWORK
-##################################################################################
-
-// module "network" {
-//   source = "./modules/network"
-
-//   vpc_cidr              = var.vpc_cidr
-//   for_tag_name          = var.for_tag_name
-// }
-
-##################################################################################
-# VARIABLES
-##################################################################################
-
-// variable "max_size" {}
-// variable "min_size" {}
-// variable "desired_capacity" {}
-// variable "instance_type" {}
-// variable "ecs_aws_ami" {}
